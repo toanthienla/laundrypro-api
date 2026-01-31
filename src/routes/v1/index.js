5c3cc4307879e04b12bb925f17142c3cf4fbfd94
Toan La <134460591+toanthienla@users.noreply.github.com>
2026-01-31 13:26:15 +0700

    Update index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { userRoute } from '~/routes/v1/userRoute';
 import { serviceRoute } from '~/routes/v1/serviceRoute';
 import { orderRoute } from '~/routes/v1/orderRoute';
+import { paymentRoute } from '~/routes/v1/paymentRoute';
 
 const Router = express.Router();
 
 Router.use('/users', userRoute);
 Router.use('/services', serviceRoute);
 Router.use('/orders', orderRoute);
+Router.use('/payments', paymentRoute);
 
 export const APIs_V1 = Router;
